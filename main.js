839e2e062247e4716f8bab65a246007002e1e203
Prem S N <prem.sn@iderize.in>
2021-06-02 19:38:51 +0530

    day 1 - added menu in app

--- main.js
+++ main.js
@@ -1,45 +1,119 @@
-// main.js
-
-// Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+process.env.NODE_ENV = 'dev'
+var isDev = process.env.NODE_ENV === 'prod' ? true : false
+var isWin = process.platform === 'win32' ? true:false
+console.log(process.platform)
+var mainWindow;
+var aboutWindow;
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 600,
-    height: 500,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+   mainWindow = new BrowserWindow({
+    title:'ImageShrink',
+    width: 700,
+    height: 700,
+    icon:`${__dirname}/src/assests/icons/Icon_256x256.png`,
+    resizable: isWin,
+    backgroundColor:'#a6d2f7'
   })
-
-  // and load the index.html of the app.
-  mainWindow.loadURL('https://dev.admin.myfayvit.com/')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //mainWindow.loadURL('https://dev.admin.myfayvit.com/')
+  mainWindow.loadFile('index.html')
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+function createAboutWindow () {
+  aboutWindow = new BrowserWindow({
+   width: 1300,
+   height: 1300,
+   resizable: false,
+   //backgroundColor:'lightgreen'
+ })
+ aboutWindow.loadURL('https://dev.admin.myfayvit.com/')
+ //aboutWindow.loadFile('about.html')
+ aboutWindow.removeMenu();
+}
 app.whenReady().then(() => {
   createWindow()
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
+  globalShortcut.register('Ctrl+R',()=>mainWindow.reload())
+  globalShortcut.register('Ctrl+i',()=>mainWindow.toggleDevTools())
+ })
+const menu = [
+  ...(isDev ? [
+    {
+      label:'File',
+      submenu:[
+        {
+          label:'Quit',
+          accelerator:'Ctrl+q',
+          click: ()=>app.quit()
+        },
+        {
+          label:'relaunch',
+          accelerator:'Ctrl+l',
+          click: ()=>app.relaunch()
+        },
+        {
+          label:'about',
+          click: ()=>app.showAboutPanel()
+        },
+        {
+          label:'showEmoji',
+          accelerator:'Ctrl+e',
+          click: ()=>app.showEmojiPanel()
+        },
+      ]
+    },
+    {
+      role:'editMenu'
+    },{
+      label:'help',
+      submenu:[{
+        label:'admin portal',
+        accelerator:'Ctrl+f',
+        click:createAboutWindow
+      }]
+    },{
+    label:'developer',
+    submenu:[
+      {role:'reload'},
+      {role:'forcereload'},
+      {type:'separator'},
+      {role:'toggledevtools'},
+    ]
+  }]:[
+    {
+      label:'File',
+      submenu:[
+        {
+          label:'Quit',
+          accelerator:'Ctrl+q',
+          click: ()=>app.quit()
+        },
+        {
+          label:'relaunch',
+          accelerator:'Ctrl+l',
+          click: ()=>app.relaunch()
+        },
+        {
+          label:'about',
+          accelerator:'Ctrl+f',
+          click: createAboutWindow
+        },
+        {
+          label:'showEmoji',
+          click: ()=>app.showEmojiPanel()
+        },
+      ]
+    }
+  ])
+]
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+//   app.on('activate', function () {
+//     // On macOS it's common to re-create a window in the app when the
+//     // dock icon is clicked and there are no other windows open.
+//     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+//   })
+// app.on('window-all-closed', function () {
+//   if (process.platform !== 'darwin') app.quit()
+// })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
